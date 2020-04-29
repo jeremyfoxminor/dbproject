35e02a8d08b24b259b083324c8c17de46ca26a56
Nolan Roe <danolanater@gmail.com>
2020-04-28 18:01:51 -0700

    Create index.php
    index.php init

---
+++ var/www/html/index.php
@@ -0,0 +1,20 @@
+<html>
+<head></head>
+        <body>
+
+                <div>
+                        <h1>Select from the drop down to view a table</h1>
+                        <form action="display_table.php" method="post">
+                        <select name="table">
+                          <option value="users">users</option>
+                          <option value="recipes">recipes</option>
+                          <option value="donations">donations</option>
+                          <option value="food">food</option>
+                          <option value="business">business</option>
+                        </select>
+                          <input type="submit" value="Submit">
+                        </form>
+                </div>
+
+        </body>
+</html>
