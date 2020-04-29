156d2b47e1d5bb12adb74d0d4ff2c30401e6978e
Nolan Roe <danolanater@gmail.com>
2020-04-28 19:19:01 -0700

    updated mysql connection credentials
    username is now "visiter" which only has SELECT permissions
    dbname has now been changed to dbproject

--- var/www/html/display_table.php
+++ var/www/html/display_table.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
+$username = "visiter";
 $password = "csc174";
-$dbname = "people";
+$dbname = "dbproject";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
