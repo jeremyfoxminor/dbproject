be206750caca1e6836efdcfcbb6efd004a9713ab
jeremyfoxminor <jeremyfoxminor@gmail.com>
2020-04-29 18:31:03 -0700

    added stored procedures and events, tweaked some things in the create_tables file

---
+++ DBProject/stored_procedures.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+
+CREATE PROCEDURE check_if_food_expired()
+BEGIN 
+	
+	UPDATE FOOD SET
+		qty = 0
+	WHERE DATEDIFF(CURDATE(), fdate) > shelf_life;
+	
+END //
+
+DELIMITER;
+
+CREATE EVENT checkExpiration
+	ON SCHEDULE EVERY 1 DAY
+	DO
+		CALL check_if_food_expired();
