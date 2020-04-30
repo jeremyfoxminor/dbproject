cf4a05d2e2bc10092104c4eec84673d12899d0a4
jeremyfoxminor <jeremyfoxminor@gmail.com>
2020-04-30 01:08:47 -0700

    Deleted unnecessary tables, added new views for keto, paleo etc, added qty to r_uses, bug tested all triggers, added more inserts for testing

--- DBProject/create_tables.sql
+++ DBProject/create_tables.sql
@@ -5,6 +5,9 @@ CREATE TABLE USER (
 	city varchar(25),
 	state char(2),
 	zip char(5),
+	mgr varchar(20),
+	btype varchar(15),
+	isBusiness boolean NOT NULL,
 	PRIMARY KEY(userid));
 	
 CREATE TABLE RECIPES (
@@ -13,6 +16,10 @@ CREATE TABLE RECIPES (
 	rtype varchar(15),
 	ravail boolean NOT NULL,
 	rdesc text,
+	keto boolean,
+	paleo boolean,
+	vegan boolean,
+	dairy_free boolean,
 	PRIMARY KEY(r_id));
 	
 CREATE TABLE FOOD (
@@ -27,24 +34,10 @@ CREATE TABLE FOOD (
 CREATE TABLE R_USES (
 	recipeid int NOT NULL,
 	foodid int NOT NULL,
+	qty int,
 	PRIMARY KEY(recipeid, foodid),
 	FOREIGN KEY(recipeid) REFERENCES RECIPES(r_id),
 	FOREIGN KEY(foodid) REFERENCES FOOD(foodid));
-	
-	
-CREATE TABLE DONATIONS (
-	userid int auto_increment NOT NULL,
-	ddate DATE NOT NULL,
-	amount DECIMAL(10,2),
-	PRIMARY KEY(userid, ddate),
-	FOREIGN KEY(userid) REFERENCES USER(userid));
-	
-CREATE TABLE BUSINESS (
-	id int auto_increment NOT NULL,
-	bname varchar(45),
-	baddress varchar(25),
-	PRIMARY KEY(id),
-	FOREIGN KEY(id) REFERENCES USER(userid));
 
 CREATE TABLE PHONE (
 	userid int auto_increment NOT NULL,
@@ -67,14 +60,26 @@ CREATE TABLE U_RECIEVES (
 	PRIMARY KEY(userid, foodid),
 	FOREIGN KEY(userid) REFERENCES USER(userid),
 	FOREIGN KEY(foodid) REFERENCES FOOD(foodid));
-	
-CREATE TABLE MV_INVENTORY AS SELECT foodid, name, qty, shelf_life, fdate FROM FOOD WHERE qty <> 0;
+
+/* Materialized Views */
+
+CREATE TABLE MV_INVENTORY AS SELECT foodid as id, name, qty, shelf_life, fdate FROM FOOD WHERE qty <> 0;
 
 CREATE TABLE MV_RECIPES AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE ravail IS TRUE;
 
+CREATE TABLE businessView AS SELECT userid, btype, mgr, name, street, city, state, zip FROM USER WHERE isBusiness is true;
+
+CREATE VIEW ketoView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE keto IS TRUE;
+
+CREATE VIEW paleoView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE paleo IS TRUE;
+
+CREATE VIEW veganView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE vegan IS TRUE;
+
+CREATE VIEW dfreeView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE dairy_free IS TRUE;
+
 CREATE VIEW R_INGREDIENTS AS 
-	SELECT r.name as Recipe, f.name as Ingredient
-	FROM RECiPES as r JOIN R_USES as u ON r.r_id = u.recipeid
+	SELECT r.name as Recipe, f.name as Ingredient, u.qty as Quantity
+	FROM RECIPES as r JOIN R_USES as u ON r.r_id = u.recipeid
 		JOIN FOOD as f ON u.foodid = f.foodid;
 	
 
--- DBProject/insert_data.sql
+++ DBProject/insert_data.sql
@@ -1,29 +1,43 @@
 INSERT INTO USER VALUES
-	(1, "Jeremy", "123 Vista", "Citrus Heights", "CA", "95610"),
-	(2, "Henry", "685 Sandy", "Los Angeles", "CA", "68123");
+	(1, "Jeremy", "123 Vista", "Citrus Heights", "CA", "95610", null, null, false),
+	(2, "Henry", "685 Sandy", "Los Angeles", "CA", "68123", null, null, false),
+	(3, "Walmart", "6969 Dusty Way", "Sacramento", "CA", "95662", null, null, true);
+
+/* THESE TWO SHOULD RETURN ERRORS */
+INSERT INTO USER VALUES
+	(4, "Test", "123 test dr.", "testcity", "CA", "12345", "hi", null, false);
 	
+INSERT INTO USER VALUES
+	(5, "Test2","123 test dr", "testme", "CA", "12345", null, "hey", false);
+/**/
+
 INSERT INTO RECIPES VALUES
-	(1, "Sick Nachos", "Mexicano", TRUE, "Put some cheese on some chips bro"),
-	(2, "Hamburger", "Americana", TRUE, "Just go to McDonalds");
+	(1, "Sick Nachos", "Mexicano", TRUE, "Put some cheese on some chips", false, false, false, false),
+	(2, "Hamburger", "Americana", TRUE, "Just go to McDonalds", false, false, false, true);
 	
 INSERT INTO FOOD VALUES
 	(1, "Tortilla Chips", "Chips", 10, "2020-04-20", 35),
 	(2, "Chedda", "Cheese", 60, "2020-04-20", 35),
 	(3, "Bun", "Bread", 10, "2020-04-20", 35),
 	(4, "Beef", "Meat", 5, "2020-04-20", 35);
-	
-INSERT INTO R_USES VALUES
-	(1, 1),
-	(1, 2),
-	(2, 3),
-	(2, 4);
-	
-INSERT INTO U_COOKS VALUES
-	(1, 1);
-	
+
+/* NO ERROR, reduces qty in foods table */
 INSERT INTO U_RECIEVES VALUES
-	(1, 1, "2020-04-30", 5),
+	(1, 1, "2020-04-30", 5);
+	
+/* NO ERROR sets qty to 0 in foods table */
+INSERT INTO U_RECIEVES VALUES
+	(1, 4, "2020-04-21", 35);
+
+/* FOOD EXPIRED ERROR */
+INSERT INTO U_RECIEVES VALUES
 	(1, 2, "2020-10-31", 7);
+	
+/* NOT ENOUGH INVENTORY */
+INSERT INTO U_RECIEVES VALUES
+	(1, 4, "2020-04-21", 40);
+	
+
 	
 
 	
