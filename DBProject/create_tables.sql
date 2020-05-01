37f89d7442eb6a713fcf75fb3f7ed3dada404c4a
Nolan Roe <danolanater@gmail.com>
2020-05-01 07:00:52 -0700

    Small tweeks to reflect MYSQL syntax vs mariadb, also got rid of ravail
    Not sure the purpose of ravail. Which recipe is available to cook should be based on user inventories and recipe ingredient requirements

--- DBProject/create_tables.sql
+++ DBProject/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS USER (
+CREATE TABLE USER (
 	userid int auto_increment NOT NULL,
 	name varchar(45),
 	street varchar(25),
@@ -10,19 +10,18 @@ CREATE TABLE IF NOT EXISTS USER (
 	isBusiness boolean NOT NULL,
 	PRIMARY KEY(userid));
 	
-CREATE TABLE IF NOT EXISTS RECIPES (
-	r_id int auto_increment NOT NULL,
+CREATE TABLE RECIPES (
+	r_id smallint auto_increment NOT NULL,
 	name varchar(45),
-	rtype varchar(15),
-	ravail boolean NOT NULL,
 	rdesc text,
-	keto boolean,
-	paleo boolean,
-	vegan boolean,
-	dairy_free boolean,
+	rtype varchar(15),
+	keto boolean default true,
+	paleo boolean default true,
+	vegan boolean default true,
+	dairy_free boolean default true,
 	PRIMARY KEY(r_id));
 	
-CREATE TABLE IF NOT EXISTS FOOD (
+CREATE TABLE FOOD (
 	foodid int auto_increment NOT NULL,
 	name varchar(45),
 	ftype varchar(15),
@@ -32,29 +31,29 @@ CREATE TABLE IF NOT EXISTS FOOD (
 	measurement char(15),
 	PRIMARY KEY(foodid));
 	
-CREATE TABLE IF NOT EXISTS R_USES (
-	recipeid int NOT NULL,
+CREATE TABLE R_USES (
+	recipeid smallint NOT NULL,
 	foodid int NOT NULL,
-	qty int,
-	measurement char(15),
+	quantity int NOT NULL,
+    	measurement enum('Count','Cup', 'Ounce', 'Pound', 'Teaspoon', 'Tablespoon') NOT NULL,
 	PRIMARY KEY(recipeid, foodid),
-	FOREIGN KEY(recipeid) REFERENCES RECIPES(r_id),
+	FOREIGN KEY(recipeid) REFERENCES RECIPES(r_id) ON DELETE CASCADE,
 	FOREIGN KEY(foodid) REFERENCES FOOD(foodid));
 
-CREATE TABLE IF NOT EXISTS PHONE (
+CREATE TABLE PHONE (
 	userid int auto_increment NOT NULL,
 	phone char(10) NOT NULL,
 	PRIMARY KEY(userid, phone),
 	FOREIGN KEY(userid) REFERENCES USER(userid));
 	
-CREATE TABLE IF NOT EXISTS U_COOKS (
+CREATE TABLE U_COOKS (
 	userid int NOT NULL,
 	recipeid int NOT NULL,
 	PRIMARY KEY(userid, recipeid),
 	FOREIGN KEY(userid) REFERENCES USER(userid),
 	FOREIGN KEY(recipeid) REFERENCES RECIPES(r_id));
 	
-CREATE TABLE IF NOT EXISTS U_RECIEVES (
+CREATE TABLE U_RECIEVES (
 	userid int NOT NULL,
 	foodid int NOT NULL,
 	date_recieved DATE,
@@ -66,23 +65,23 @@ CREATE TABLE IF NOT EXISTS U_RECIEVES (
 
 /* Materialized Views */
 
-CREATE TABLE IF NOT EXISTS MV_INVENTORY AS SELECT foodid as id, name, qty, measurement, shelf_life, fdate FROM FOOD WHERE qty <> 0;
+CREATE TABLE MV_INVENTORY AS SELECT foodid as id, name, qty, measurement, shelf_life, fdate FROM FOOD WHERE qty <> 0;
 
-CREATE TABLE IF NOT EXISTS MV_RECIPES AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE ravail IS TRUE;
+/* CREATE TABLE MV_RECIPES AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE ravail IS TRUE; */
 
-CREATE TABLE IF NOT EXISTS businessView AS SELECT userid, btype, mgr, name, street, city, state, zip FROM USER WHERE isBusiness is true;
+CREATE TABLE businessView AS SELECT userid, btype, mgr, name, street, city, state, zip FROM USER WHERE isBusiness is true;
 
-CREATE VIEW IF NOT EXISTS ketoView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE keto IS TRUE;
+CREATE VIEW ketoView AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE keto IS TRUE;
 
-CREATE VIEW IF NOT EXISTS paleoView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE paleo IS TRUE;
+CREATE VIEW paleoView AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE paleo IS TRUE;
 
-CREATE VIEW IF NOT EXISTS veganView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE vegan IS TRUE;
+CREATE VIEW veganView AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE vegan IS TRUE;
 
-CREATE VIEW IF NOT EXISTS dfreeView AS SELECT r_id, name, rtype, ravail, rdesc FROM RECIPES WHERE dairy_free IS TRUE;
-
-CREATE VIEW IF NOT EXISTS R_INGREDIENTS AS 
-	SELECT r.name as Recipe, f.name as Ingredient, u.qty as Quantity
-	FROM RECIPES as r JOIN R_USES as u ON r.r_id = u.recipeid
+CREATE VIEW dfreeView AS SELECT r_id, name, rtype, rdesc FROM RECIPES WHERE dairy_free IS TRUE;
+		
+CREATE VIEW R_INGREDIENTS AS 
+	SELECT r.name as Recipe, u.quantity as Quantity, u.measurement as Measurement, f.name as Ingredient 
+	FROM RECIPES as r JOIN R_USES as u ON r.r_id = u.recipeid 
 		JOIN FOOD as f ON u.foodid = f.foodid;
 	
 
